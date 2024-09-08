f48e4ea4992f6c0e1953e727b23717a581ff2a5c
a.schurf@gmail.com <a.schurf@gmail.com>
2024-09-08 13:08:49 +0300

    234234

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -11,8 +11,6 @@ import 'package:flutter_guid/flutter_guid.dart';
 import 'dart:async';
 import 'dart:io';
 import 'package:http/http.dart' as http;
-import 'package:media_kit/media_kit.dart';
-import 'package:media_kit_video/media_kit_video.dart';
 
 
 
@@ -100,10 +98,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  // Create a [Player] to control playback.
-  late final player = Player();
-  // Create a [VideoController] to handle video output from [Player].
-  late final controller = VideoController(player);
+
 
   List<OrderDishesModel> orderDishes = [];
 
@@ -117,7 +112,6 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     // initial load
     _listFuture = getMenus(widget.menuId, '7ae08cea-95e9-4136-a746-ed0fe8077770');
-    // player.open(Media('https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4'));
 
   }
 
@@ -129,7 +123,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void dispose() {
-    player.dispose();
     super.dispose();
   }
 
